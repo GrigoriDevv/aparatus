ff76e7074def9ebe426b8795064b463d2844f115
Gabriel Santos <gabrielgrigori8@gmail.com>
2025-11-10 17:23:45 -0300

    feature use prisma client with backend connect

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,11 @@
+import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
-import "dotenv/config";   
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    seed: "tsx primsa/seed.ts"
+    seed: "tsx prisma/seed.ts",
   },
   engine: "classic",
   datasource: {
