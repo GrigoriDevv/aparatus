dd1c17f49955d859e2cc761f33e3a75fac38e4cb
Gabriel Santos <gabrielgrigori8@gmail.com>
2025-11-11 15:32:02 -0300

    create header and search input

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,11 +1,15 @@
+
 import Image from "next/image";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
 
 const Header = () => {
     return(
-    <header className="flex items-center justify-between px-4 py-6">
-        <Image src="/logo.png" alt="Aparatus Logo"/>
-
-
+    <header className="flex items-center justify-between bg-white shadow-md px-5 py-6">
+      <Image src="/logo.svg" alt="Aparatus" width={100} height={26.09} />
+         <Button variant="outline" size="icon">
+          <MenuIcon  />
+        </Button>
     </header>)
 }
 
