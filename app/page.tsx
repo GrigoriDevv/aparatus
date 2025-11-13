58c1cf3f5ace52814a1f851186fc9d1d583b37c1
Gabriel Santos <gabrielgrigori8@gmail.com>
2025-11-13 18:28:25 -0300

    feat: create in the home, secion booking and barbers

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,49 @@ import SearchInput from "./_components/search-input";
 import Image from "next/image";
 import Frame from "@/public/Frame.png";
 import BookingItem from "./_components/booking-item";
+import { prisma } from "@/lib/prisma";
+import { Barbershop } from "@prisma/client";
+import BarbershopItem from "./_components/barbershop-item";
 
 
- const Home = () =>  {
+ const Home =  async () =>  {
+
+  const barbershops = await prisma.barbershop.findMany()
+
   return (
-    <main className="bg-zinc-50 font-sans dark:bg-black">
+    <main className="bg-zinc-50 font-sans dark:bg-black p-5">
       <Header />
       <div className="space-y-4 px-5 py-6">
         <SearchInput />
         <Image src={Frame}
-         alt="Agende agora"
-         sizes="100vw"
-         className="w-full h-auto" />
+          alt="Agende agora"
+          sizes="100vw"
+          className="w-full h-auto" />
          <h2 className="text-xs font-semibold uppercase text-foreground">Agendamentos</h2>
          <BookingItem 
-         serviceName="Corte de cabelo"
-         barbershopName="Barbearia do João"
-         barbershopImageUrl="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
-         date={new Date()}
+          serviceName="Corte de cabelo"
+          barbershopName="Barbearia do João"
+          barbershopImageUrl="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
+          date={new Date()}
          />
+         {barbershops.map((barbershop: Barbershop) => (
+          <BookingItem
+            key={barbershop.id}
+            serviceName="Corte de cabelo"
+            barbershopName={barbershop.name}
+            barbershopImageUrl={barbershop.imageUrl}
+            date={new Date()}
+          />
+         ))}
+         <h2 className="text-xs font-semibold uppercase text-foreground">Barbearias</h2>
+        <div className="flex gap-2 overflow-x-auto w-full [&::-webkit-scrollbar]:hidden">
+        {barbershops.map((barbershop: Barbershop) => (
+            <BarbershopItem
+              key={barbershop.id} 
+              barbershop={barbershop}
+            />
+          ))}
+        </div>
       </div>
     </main>
   );
